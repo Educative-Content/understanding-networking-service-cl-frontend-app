8d0b8a0fe2c08165015edadf9d0b8323ddf2088a
Haziq Ali <60395311+theHaziqali@users.noreply.github.com>
2023-09-18 16:56:39 +0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+  const[info, setInfo]=useState(['loading...'])
+
+  useEffect(() => {
+    const information= axios.get('http://localhost:3000/api/books')
+      .then(response => {
+        setData(response.data);
+        setInfo(information.data)
+        console.log(information.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Data from API</h1>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>{item.title}{item.author}</li>
+        ))}
+      </ul>
     </div>
   );
 }
