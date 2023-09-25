061d2b4d1c773b29b4668046d7efb29e90ad9737
Haziq Ali <60395311+theHaziqali@users.noreply.github.com>
2023-09-25 10:26:00 +0500

    updated middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://YOUR_PRIVATE_IP_ADDRESS:PORT', // Specify the target URL where your API is hosted
+      target: 'http://<YOUR_PRIVATE_IP_ADDRESS>:3000', // Specify the target URL where your API is hosted
       changeOrigin: true,
     })
   );
